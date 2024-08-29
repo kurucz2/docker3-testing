788173cdaee286e1f694fe7ec06d34aefec886a9
kurucz2 <kuruczbalint215@outlook.com>
2024-08-29 08:50:24 -0700

    Update index.js

--- files/index.js
+++ files/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 3000;        
-const UUID = process.env.UUID || '986e0d08-b275-4dd3-9e75-f3094b36fa2a'; //若需要改UUID，需要在config.json里改为一致
-const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
-const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
-const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.zzx.free.hr';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOGI5NzI0MDgwZTU1ZTcwMzcwZmI3NDI4NzkyMmYzMWIiLCJ0IjoiOGNlY2VlYzQtYzZiNi00N2VkLThhZjItY2I4MThmMDkxZWJkIiwicyI6Ik5XWTFNV1ZsWm1NdFpEYzJZeTAwWkdSaExUbGtZall0TnpneVpqZ3haVE00WkRBNSJ9';
-const CFIP = process.env.CFIP || 'na.ma';
+const UUID = process.env.UUID || '13d41525-5675-44dd-a8e5-78efa70731cf'; 
+const NEZHA_SERVER = process.env.NEZHA_SERVER || '';     
+const NEZHA_PORT = process.env.NEZHA_PORT || '5555'; 
+const NEZHA_KEY = process.env.NEZHA_KEY || '';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'server-1.sandrahavens.cloudns.be'; 
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYTQ0YzkyYWZlZWE2NGVjYjYzNDcxYTJiZTA4ZTRjMjciLCJ0IjoiMmYwMWE0MTItNWY1ZC00MjFhLTgzMDAtODE4YzE3NjM2YWUxIiwicyI6IlkyTXlOR1ptTURVdE5tRmxOeTAwWVdFd0xXSTRNbVF0TXpKaU4yUmxZV1E1WkdVdyJ9';
+const CFIP = process.env.CFIP || 'ip.sb';
 const NAME = process.env.NAME || 'Choreo';
 
 // root route
